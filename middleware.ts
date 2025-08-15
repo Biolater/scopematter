43cf7b1188bd631c67dace33349565928ad68b58
Murad <yusifovmurad1@gmail.com>
2025-08-16 02:46:38 +0400

    refactor: update middleware to use async authentication
    - Changed the authentication function in middleware to be asynchronous, ensuring proper handling of protected routes.
    - Updated the route protection logic to await the authentication process for better reliability.

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware( async (auth, req) => {
+  if (isProtectedRoute(req) ) {
+    await auth();
+  }
 });
 
 export const config = {
