c1f5da3877114d11878199a46e17252ddcb1ce0a
Murad <yusifovmurad1@gmail.com>
2025-08-16 23:57:12 +0400

    Show profile button on mobile navbar and protect routes

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,25 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware( async (auth, req) => {
-  if (isProtectedRoute(req) ) {
-    await auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (userId && isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!userId && !isPublicRoute(req)) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 });
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
