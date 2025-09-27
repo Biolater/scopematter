f414f03c54c12ea3ca48ed60ccf6e2417f9c3a8a
Murad <yusifovmurad1@gmail.com>
2025-09-27 09:38:23 +0300

    Remove payment and wallet modules

--- lib/data/wallet.ts
+++
@@ -1,9 +0,0 @@
-import { handleQuery } from "../http/query";
-import { Wallet } from "../types/wallet.types";
-
-export const getWallets = async () => {
-    return await handleQuery<Wallet[]>("/wallets", {
-        tags: ["wallets"],
-        revalidate: 300,
-    })
-};
--- lib/utils/wallet.utils.ts
+++
@@ -1,4 +0,0 @@
-export function shortAddr(addr: string) {
-  if (!addr || addr.length < 12) return addr;
-  return `${addr.slice(0, 6)}…${addr.slice(-4)}`;
-}
