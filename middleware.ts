d2514181a0a54e35fa404004e21489ebdbc7113b
Murad <yusifovmurad1@gmail.com>
2025-10-08 10:42:51 +0300

    feat: enhance HeroSection with video demo and improve layout
    - Added a video demo to the HeroSection for better user engagement.
    - Refactored layout of HeroSection components for improved readability and structure.
    - Updated middleware to exclude mp4 files from static file handling for better performance.

--- middleware.ts
+++ middleware.ts
@@ -28,11 +28,10 @@ export default clerkMiddleware(async (auth, req) => {
   signUpUrl: env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
 })
 
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|mp4)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
